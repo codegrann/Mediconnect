8f51b96dc7693c5f6e65896659b789a10970127d
codegrann <omondihills02@gmail.com>
2024-03-12 13:21:39 +0300

    .

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -5,7 +5,10 @@ import ServiceCard2 from "../Minor/ServiceCard2";
 function Services() {
   return (
     <div className="mt-6 pb-10 md:w-5/6 mx-auto">
-      <h3 className="text-black font-medium md:text-xl md:mb-10" id="services">
+      <h3
+        className="text-black font-medium md:text-xl mb-6 md:mb-10"
+        id="services"
+      >
         Services
       </h3>
       {/* <div className=" w-7/8 mx-auto md:flex md:flex-wrap md:justify-center md:gap-6 lg:w-full lg:justify-center">
