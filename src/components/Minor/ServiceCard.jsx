3c0cd7fcb4693b6cdc275ab4e0618f10a60b68b4
codegrann <omondihills02@gmail.com>
2024-03-12 13:35:25 +0300

    .

--- src/components/Minor/ServiceCard.jsx
+++ src/components/Minor/ServiceCard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import ClickForward from "./Actions/ClickForward";
 
 function ServiceCard({ serviceName, serviceDescription }) {
+  const navigate = useNavigate();
   return (
     <div className="max-w-80 mb-6 mx-auto border-2 border-solid border-gray-300 px-4 py-8 lg:w-1/4 rounded-3xl">
       <h4 className="mb-4 text-zinc-700 font-semibold">{serviceName}</h4>
       <p className="text-sm/6 text-zinc-900 mb-12"> {serviceDescription}</p>
-      <ClickForward label={`Learn more about ${serviceName}`} />
+      {/* <a href="/#services" className="border-2 border-solid border-black"> */}
+      <ClickForward
+        label={`Learn more about ${serviceName}`}
+        onclick={navigate("/#services")}
+      />
+      {/* </a> */}
     </div>
   );
 }
--- src/components/Minor/ServiceCard2.jsx
+++ src/components/Minor/ServiceCard2.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import ClickForward from "./Actions/ClickForward";
 import ImageContainer from "./ImageContainer";
 
 function ServiceCard2({ title, imageSrc, description, buttonText }) {
+  const navigate = useNavigate();
   return (
     <div className="mb-6 max-w-80 mx-auto lg: border-none p-0.5 bg-gradient-to-r from-[#f83904] via-black to-[#2bf607] rounded-3xl hover:animate-moveColors items-center">
       <div className="py-6 flex flex-col gap-4 items-center justify-center bg-white rounded-3xl p-1 sm:p-2">
@@ -11,7 +14,16 @@ function ServiceCard2({ title, imageSrc, description, buttonText }) {
 
         <h3 className="text-[#092A9F] font-medium">{title}</h3>
         <p className="text-center text-xs sm:text-sm">{description}</p>
-        {buttonText ? <ClickForward label={buttonText} /> : ""}
+        {buttonText ? (
+          <a href="/#services" className="border-2 border-solid border-black">
+            <ClickForward
+              label={buttonText}
+              onclick={() => navigate("/#services")}
+            />
+          </a>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
