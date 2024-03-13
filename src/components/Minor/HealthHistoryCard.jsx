65d5c3ee31f6d6a7cf91a34ac165a06c232bae77
codegrann <omondihills02@gmail.com>
2024-03-13 07:33:03 +0300

    .

--- src/components/Minor/HealthHistoryCard.jsx
+++ src/components/Minor/HealthHistoryCard.jsx
@@ -14,17 +14,31 @@ function HealthHistory({ history }) {
     <div className="text-sm/6 p-2 border-2 mb-4 md:w-3/4 lg:w-1/2 lg:mx-auto border-gray-400 rounded-sm">
       <div>
         <p className=" flex flex-wrap gap-x-4">
-          <span>Facility: {facility}</span>
-          <span>Date: {date}</span>
+          <span>
+            <b>Facility:</b> {facility}
+          </span>
+          <span>
+            <b>Date:</b> {date}
+          </span>
         </p>
-        <p className=" flex flex-wrap gap-4">{symptoms}</p>
         <p className=" flex flex-wrap gap-4">
-          <span>{diagnosis}</span>
-          <span>{treatment}</span>
+          <b>Symptoms:</b> {symptoms}
         </p>
         <p className=" flex flex-wrap gap-4">
-          <span>{providerName}</span>
-          <span>{regno}</span>
+          <span>
+            <b>Diagnosis:</b> {diagnosis}
+          </span>
+          <span>
+            <b>Treatment:</b> {treatment}
+          </span>
+        </p>
+        <p className=" flex flex-wrap gap-4">
+          <span>
+            <b>Provider:</b> {providerName}
+          </span>
+          <span>
+            <b>Reg no:</b> {regno}
+          </span>
         </p>
       </div>
     </div>
